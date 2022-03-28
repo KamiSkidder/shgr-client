a704bdb0bc2701111a10f07c92225d9e2059414a
KamiSkidder <87556065+KamiSkidder@users.noreply.github.com>
2022-03-28 16:39:10 +0900

    CustomFovとrpcいじり
    Enjoying

--- src/main/java/com/kamiskidder/shgr/module/render/CustomFov.java
+++ src/main/java/com/kamiskidder/shgr/module/render/CustomFov.java
@@ -7,7 +7,7 @@ import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class CustomFov extends Module {
-    public Setting<Float> fov = new Setting<Float>("Fov", 70.0f);
+    public Setting<Float> fov = register(new Setting<Float>("Fov", 70.0f));
 
     public CustomFov() {
         super("CustomFov", Category.RENDER);
