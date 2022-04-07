9ef129eb187e6d98368c40cc355f9c8f6b9fa67d
KamiSkidder <87556065+KamiSkidder@users.noreply.github.com>
2022-04-07 23:19:21 +0900

    added swordrotator

--- src/main/java/com/kamiskidder/shgr/mixin/mixins/MixinMinecraft.java
+++ src/main/java/com/kamiskidder/shgr/mixin/mixins/MixinMinecraft.java
@@ -3,22 +3,30 @@ package com.kamiskidder.shgr.mixin.mixins;
 import com.kamiskidder.shgr.event.client.KeyboardUpdateEvent;
 import com.kamiskidder.shgr.manager.ConfigManager;
 import com.kamiskidder.shgr.manager.FriendManager;
+import com.kamiskidder.shgr.module.combat.AutoGaiji;
 import com.kamiskidder.shgr.ui.mainmenu.GuiCustomMainMenu;
 import com.kamiskidder.shgr.util.Util;
 import com.kamiskidder.shgr.util.client.EventUtil;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.settings.GameSettings;
+import org.lwjgl.LWJGLException;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.io.IOException;
 
 @Mixin(value = Minecraft.class)
 public abstract class MixinMinecraft implements Util {
     @Shadow
     public GameSettings gameSettings;
+
+    @Shadow protected abstract void init() throws LWJGLException, IOException;
 
     @Inject(method = "runTick()V", at = @At("HEAD"))
     public void onKeyboardUpdate(CallbackInfo ci) {
@@ -36,5 +44,19 @@ public abstract class MixinMinecraft implements Util {
     public void shutdown(CallbackInfo info) {
         ConfigManager.save();
         FriendManager.save();
+    }
+
+    @Redirect(method = "processKeyBinds()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/entity/EntityPlayerSP;isHandActive()Z"))
+    public boolean isHandActive(EntityPlayerSP instance) {
+        if (AutoGaiji.INSTANCE.isToggled())
+            return false;
+        return instance.isHandActive();
+    }
+
+    @Redirect(method = "processKeyBinds()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;rightClickMouse()V"))
+    public void rightClickMouse(Minecraft instance) {
+        if (AutoGaiji.INSTANCE.isToggled())
+            return;
+        instance.rightClickMouse();
     }
 }
--- src/main/java/com/kamiskidder/shgr/module/combat/AutoGaiji.java
+++ src/main/java/com/kamiskidder/shgr/module/combat/AutoGaiji.java
@@ -1,5 +1,14 @@
 package com.kamiskidder.shgr.module.combat;
 
+import java.awt.Color;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import org.lwjgl.input.Mouse;
+
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 import com.kamiskidder.shgr.event.player.UpdateLivingPlayerEvent;
 import com.kamiskidder.shgr.event.player.UpdatePlayerMoveStateEvent;
 import com.kamiskidder.shgr.manager.ModuleManager;
@@ -10,25 +19,34 @@ import com.kamiskidder.shgr.module.Setting;
 import com.kamiskidder.shgr.module.movement.Scaffold;
 import com.kamiskidder.shgr.util.client.MathUtil;
 import com.kamiskidder.shgr.util.entity.EntityUtil;
+import com.kamiskidder.shgr.util.player.BlockUtil;
 import com.kamiskidder.shgr.util.player.PlayerUtil;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemBow;
+import net.minecraft.util.EntitySelectors;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
-import java.awt.*;
 
 public class AutoGaiji extends Module {
     public static AutoGaiji INSTANCE;
     public Setting<Float> distance = register(new Setting("Distance", 15.0f, 30.0f, 5.0f));
+    public Setting<Boolean> mct = register(new Setting("MiddleClick", true));
+    public Setting<Boolean> right = register(new Setting("Right", false, v -> mct.getValue()));
     public Setting<Boolean> scaffold = register(new Setting("Scaffold", true));
     public Setting<Boolean> render = register(new Setting("Render", true));
     public Setting<Color> color = register(new Setting("Color", new Color(255, 0, 0, 100), v -> render.getValue()));
     public EntityPlayer target = null;
     private int randomX, randomZ = 0;
     private boolean annoy = false;
+    private boolean clicked = false;
+
     public AutoGaiji() {
         super("AutoGaiji", Category.COMBAT);
         INSTANCE = this;
@@ -39,15 +57,42 @@ public class AutoGaiji extends Module {
         Scaffold module = (Scaffold) ModuleManager.INSTANCE.getModuleByClass(Scaffold.class);
         if (module.isToggled() && scaffold.getValue())
             module.toggle();
-
+        
+        target = null;
         mc.gameSettings.keyBindUseItem.pressed = false;
     }
 
     @Override
     public void onTick() {
+        if (mct.getValue()) {
+            boolean mouse = (Mouse.isButtonDown(2) || (right.getValue() && Mouse.isButtonDown(1)));
+            if (!clicked && mouse) {
+                clicked = true;
+                onMouseClicked();
+            } else if (clicked && !mouse) {
+                clicked = false;
+            }
+        }
+
         if (target == null) return;
+        //reset
+        if (target.isDead || target.getHealth() < 0.1f) {
+            target = null;
+            sendMessage("The Target already dead or unloaded!");
+            disable();
+            return;
+        }
 
         annoy = !PlayerUtil.canSeeEntity(target);
+    }
+
+    public void onMouseClicked() {
+        Entity entityHit = getMouseOver(1.0f);
+
+        if (entityHit instanceof EntityPlayer) {
+            target = (EntityPlayer) entityHit;
+            sendMessage("Target set to " + target.getDisplayNameString());
+        }
     }
 
     @SubscribeEvent
@@ -62,6 +107,10 @@ public class AutoGaiji extends Module {
         if (mc.player.posY < 74 || !mc.player.onGround)
             return;
 
+        //bruhmoment
+        if (BlockUtil.getBlock(EntityUtil.getEntityPos(target).add(0, 2, 0)) != Blocks.AIR)
+            return;
+
         boolean cansee = true;
         if (getDistance(target) > distance.getValue() || annoy) {
             cansee = false;
@@ -73,7 +122,7 @@ public class AutoGaiji extends Module {
 
             if (annoy
                     && PlayerUtil.getDistance(new BlockPos(pos.getX(), mc.player.posY, pos.getZ())) < 5) {
-                int dis = distance.getValue().intValue() + 1;
+                int dis = distance.getValue().intValue() / 2;
                 randomX = dis * MathUtil.getRandom(-1, 1);
                 randomZ = dis * MathUtil.getRandom(-1, 1);
             }
@@ -109,22 +158,25 @@ public class AutoGaiji extends Module {
 
             RotateManager.lookAtEntity(target);
 
-            if (mc.currentScreen == null)
-                mc.gameSettings.keyBindUseItem.pressed = true;
+            if (!mc.player.isHandActive()) {
+                mc.rightClickMouse();
+            }
 
             if (72000 - mc.player.getItemInUseCount() > 24)
                 mc.playerController.onStoppedUsingItem(mc.player);
         }
 
         //stopping
-        if (Math.abs(mc.player.motionX) < 0.05 && Math.abs(mc.player.motionY) < 0.05 && Math.abs(mc.player.motionZ) < 0.05) {
-            //BlockPos pos = PlayerUtil.getPlayerPos();
-            //mc.player.setPosition(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
+        if (Math.abs(mc.player.motionX) < 0.02 && Math.abs(mc.player.motionY) < 0.02 && Math.abs(mc.player.motionZ) < 0.02) {
+            BlockPos pos = PlayerUtil.getPlayerPos();
+            mc.player.setPosition(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
         }
     }
 
     @SubscribeEvent
     public void onUpdatePlayerMoveState(UpdatePlayerMoveStateEvent event) {
+        if (target == null) return;
+
         if (!(mc.player.posY > 74)) {
             mc.player.movementInput.jump = true;
         }
@@ -134,5 +186,57 @@ public class AutoGaiji extends Module {
         float f = (float) (mc.player.posX - entityIn.posX);
         float f2 = (float) (mc.player.posZ - entityIn.posZ);
         return MathHelper.sqrt(f * f + f2 * f2);
+    }
+
+    private Entity getMouseOver(float partialTicks) {
+        Entity entity = mc.player;
+
+        if (entity != null) {
+            if (this.mc.world != null) {
+                double d0 = 100;
+                double d1 = d0;
+                Vec3d vec3d = entity.getPositionEyes(partialTicks);
+                Vec3d vec3d1 = entity.getLook(1.0F);
+                Vec3d vec3d2 = vec3d.add(vec3d1.x * d0, vec3d1.y * d0, vec3d1.z * d0);
+                float f = 1.0F;
+                List<Entity> list = this.mc.world.getEntitiesInAABBexcluding(entity, entity.getEntityBoundingBox().expand(vec3d1.x * d0, vec3d1.y * d0, vec3d1.z * d0).grow(1.0D, 1.0D, 1.0D), Predicates.and(EntitySelectors.NOT_SPECTATING, new Predicate<Entity>() {
+                    public boolean apply(@Nullable Entity p_apply_1_) {
+                        return p_apply_1_ != null && p_apply_1_.canBeCollidedWith();
+                    }
+                }));
+                double d2 = d1;
+
+                Entity pointedEntity = null;
+                for (int j = 0; j < list.size(); ++j) {
+                    Entity entity1 = list.get(j);
+                    AxisAlignedBB axisalignedbb = entity1.getEntityBoundingBox().grow((double) entity1.getCollisionBorderSize());
+                    RayTraceResult raytraceresult = axisalignedbb.calculateIntercept(vec3d, vec3d2);
+
+                    if (axisalignedbb.contains(vec3d)) {
+                        if (d2 >= 0.0D) {
+                            pointedEntity = entity1;
+                            d2 = 0.0D;
+                        }
+                    } else if (raytraceresult != null) {
+                        double d3 = vec3d.distanceTo(raytraceresult.hitVec);
+
+                        if (d3 < d2 || d2 == 0.0D) {
+                            if (entity1.getLowestRidingEntity() == entity.getLowestRidingEntity() && !entity1.canRiderInteract()) {
+                                if (d2 == 0.0D) {
+                                    pointedEntity = entity1;
+                                }
+                            } else {
+                                pointedEntity = entity1;
+                                d2 = d3;
+                            }
+                        }
+                    }
+                }
+
+                return pointedEntity;
+            }
+        }
+
+        return null;
     }
 }
