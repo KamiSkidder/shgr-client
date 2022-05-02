cbe280815b830a55709334a0c6c07258aa733ab4
KamiSkidder <87556065+KamiSkidder@users.noreply.github.com>
2022-05-02 14:10:33 +0900

    Update AutoCrystal
    フレンドに攻撃しないのと
    ひとり狙い問題修正

--- src/main/java/com/kamiskidder/shgr/util/entity/EntityUtil.java
+++ src/main/java/com/kamiskidder/shgr/util/entity/EntityUtil.java
@@ -1,5 +1,6 @@
 package com.kamiskidder.shgr.util.entity;
 
+import com.kamiskidder.shgr.manager.FriendManager;
 import com.kamiskidder.shgr.util.Util;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
@@ -11,6 +12,10 @@ import java.util.stream.Collectors;
 public class EntityUtil implements Util {
     public static List<EntityPlayer> getPlayers() {
         return mc.world.playerEntities.stream().filter(e -> e.entityId != mc.player.entityId).collect(Collectors.toList());
+    }
+
+    public static List<EntityPlayer> getEnemyPlayers() {
+        return getPlayers().stream().filter(e -> !FriendManager.isFriend(e)).collect(Collectors.toList());
     }
 
     public static EntityPlayer getPlayerByName(String name) {
